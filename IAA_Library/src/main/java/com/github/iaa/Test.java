129005326768dbe22832b5c10e13e535d078e169
ttt <tttt>
2024-12-09 14:03:43 +0800

    Initial commit

---
+++ IAA_Library/src/main/java/com/github/iaa/Test.java
@@ -0,0 +1,12 @@
+package com.github.iaa;
+
+import android.util.Log;
+
+public class Test {
+
+    public static void logTest(String msg){
+        Log.e("IAA","Message:"+msg);
+    }
+
+
+}
---
+++ app/src/main/java/io/mini/play/iaa_sdk/MainActivity.java
@@ -0,0 +1,22 @@
+package io.mini.play.iaa_sdk;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.iaa.Test;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Test.logTest("12345");
+    }
+}
